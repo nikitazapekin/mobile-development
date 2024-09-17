966c3d2df91d0cc5a1d621e35638b3464d8dfec7
nikitazapekin <wotblitz362@mail.ru>
2024-09-17 12:08:52 +0300

    display error message

--- app/src/main/java/com/example/lab_2/MainActivity.java
+++ app/src/main/java/com/example/lab_2/MainActivity.java
@@ -1,4 +1,48 @@
+package com.example.lab_2;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void handleClick(View view) {
+        EditText numberInput = findViewById(R.id.numberInput);
+        TextView errorTextView = findViewById(R.id.error);
+
+        String inputText = numberInput.getText().toString();
+        if (inputText.isEmpty()) {
+
+            errorTextView.setVisibility(View.VISIBLE);
+        } else {
+            errorTextView.setVisibility(View.GONE);
+            Toast.makeText(this, "Вы ввели: " + inputText, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
+
+
+
+/*
 package com.example.lab_2;
 
 import android.os.Bundle;
@@ -29,51 +73,4 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
     }
 }
-
-/*
-
-
-package com.example.lab_2;
-
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-
-
-
-
-
-
-
-
-
-import android.view.View;
-import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
-        public void handleClick(View view) {
-            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
-        }
-    }
-}
 */
-

683dc98323f4fe131afb0ebc91268f647d97184a
nikitazapekin <wotblitz362@mail.ru>
2024-09-22 11:46:07 +0300

    remove code smeels

--- app/src/main/java/com/example/lab_3/SecondActivity.java
+++ app/src/main/java/com/example/lab_3/SecondActivity.java
@@ -1,6 +1,6 @@
 package com.example.lab_3;
 
-import android.content.Intent; // Добавьте этот импорт
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,63 +11,12 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second);
-
-        // Получаем Intent
-        Intent intent = getIntent();  // Этот метод извлекает Intent, который запустил активность
-
-        // Извлекаем данные из Intent
-        String firstName = intent.getStringExtra("FIRST_NAME");
-        String lastName = intent.getStringExtra("LAST_NAME");
-        String phone = intent.getStringExtra("PHONE");
-
-        // Найдем TextView и установим текст
-        TextView textView = findViewById(R.id.textView);
-        textView.setText("Имя: " + firstName + "\nФамилия: " + lastName + "\nТелефон: " + phone);
-    }
-}
-
-
-/*
-package com.example.lab_3;
-
-import android.os.Bundle;
-import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
-
-public class SecondActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.second);
-
-        // Получаем Intent
         Intent intent = getIntent();
-
-        // Извлекаем данные из Intent
         String firstName = intent.getStringExtra("FIRST_NAME");
         String lastName = intent.getStringExtra("LAST_NAME");
         String phone = intent.getStringExtra("PHONE");
-
-        // Найдем TextView и установим текст
         TextView textView = findViewById(R.id.textView);
         textView.setText("Имя: " + firstName + "\nФамилия: " + lastName + "\nТелефон: " + phone);
     }
 }
-*/
 
-/*
-package com.example.lab_3;
-
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-
-public class SecondActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.second);
-    }
-}
-*/

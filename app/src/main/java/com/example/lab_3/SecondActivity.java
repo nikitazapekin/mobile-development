319a95058bb7c381c6a9d7710a0fcc7fdd9dd32b
nikitazapekin <wotblitz362@mail.ru>
2024-09-22 12:54:33 +0300

    third activity

--- app/src/main/java/com/example/lab_3/SecondActivity.java
+++ app/src/main/java/com/example/lab_3/SecondActivity.java
@@ -2,8 +2,12 @@ package com.example.lab_3;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -18,5 +22,16 @@ public class SecondActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.textView);
         textView.setText("Имя: " + firstName + "\nФамилия: " + lastName + "\nТелефон: " + phone);
     }
+
+
+
+
+
+public void handleRedirect(View view) {
+    Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+    startActivity(intent);
 }
 
+
+
+}
---
+++ app/src/main/java/com/example/lab_3/ThirdActivity.java
@@ -0,0 +1,69 @@
+
+package com.example.lab_3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third);
+
+
+    }
+
+
+
+
+}
+
+
+/*
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button2);
+        EditText firstName = findViewById(R.id.firstName);
+        EditText lastName = findViewById(R.id.lastName);
+        EditText phone = findViewById(R.id.phone);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String firstNameText = firstName.getText().toString();
+                String lastNameText = lastName.getText().toString();
+                String phoneText = phone.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+                intent.putExtra("FIRST_NAME", firstNameText);
+                intent.putExtra("LAST_NAME", lastNameText);
+                intent.putExtra("PHONE", phoneText);
+
+
+                startActivity(intent);
+            }
+        });
+    }
+
+
+package com.example.lab_3;
+
+public class ThirdActivity {
+
+}
+
+*/

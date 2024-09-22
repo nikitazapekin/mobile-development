ad95d64043e80740e3fa1fda151d58d97f24eabb
nikitazapekin <wotblitz362@mail.ru>
2024-09-22 11:44:16 +0300

    add transform data from one screen to another

--- app/src/main/java/com/example/lab_3/MainActivity.java
+++ app/src/main/java/com/example/lab_3/MainActivity.java
@@ -1,3 +1,48 @@
+
+package com.example.lab_3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button2);
+        EditText firstName = findViewById(R.id.firstName);
+        EditText lastName = findViewById(R.id.lastName);
+        EditText phone = findViewById(R.id.phone);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String firstNameText = firstName.getText().toString();
+                String lastNameText = lastName.getText().toString();
+                String phoneText = phone.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+                intent.putExtra("FIRST_NAME", firstNameText);
+                intent.putExtra("LAST_NAME", lastNameText);
+                intent.putExtra("PHONE", phoneText);
+
+
+                startActivity(intent);
+            }
+        });
+    }
+}
+
+
+/*
 package com.example.lab_3;
 
 import android.content.Intent;
@@ -24,33 +69,6 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                 startActivity(intent);
             }
-        });
-    }
-}
-
-
-/*
-package com.example.lab_3;
-
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
         });
     }
 }

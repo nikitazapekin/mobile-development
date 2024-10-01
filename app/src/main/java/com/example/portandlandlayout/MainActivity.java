0af13911aea11e20f083f11d39399b5a2c7cdbe9
nikitazapekin <wotblitz362@mail.ru>
2024-10-01 20:03:54 +0300

    fix some changes

--- app/src/main/java/com/example/portandlandlayout/MainActivity.java
+++ app/src/main/java/com/example/portandlandlayout/MainActivity.java
@@ -1,4 +1,157 @@
 package com.example.portandlandlayout;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.res.Configuration;
+import androidx.annotation.NonNull;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String color;
+    private static final String TAG = "MainActivity";
+    private static final String COLOR_KEY = "editTextColorText";
+    public static final String PREFS_FILE = "screenData";
+
+    SharedPreferences screenData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_SHORT).show();
+
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button saveButton = findViewById(R.id.button4);
+        Button loadButton = findViewById(R.id.button5);
+
+        button1.setOnClickListener(this::handleClick);
+        button2.setOnClickListener(this::handleClick);
+        button3.setOnClickListener(this::handleClick);
+        saveButton.setOnClickListener(v -> saveSelectedColor(color));
+        loadButton.setOnClickListener(v -> applySavedColor(color));
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+         screenData = getSharedPreferences(PREFS_FILE, MODE_PRIVATE);
+          color = screenData.getString(COLOR_KEY, "");
+    //   applySavedColor(color);
+
+        if (savedInstanceState != null) {
+            Toast.makeText(this, savedInstanceState.getString(COLOR_KEY, ""), Toast.LENGTH_SHORT).show();
+        }
+
+        Log.d(TAG, "onCreate");
+    }
+
+    public void handleClick(View v) {
+        View square1 = findViewById(R.id.green_square1);
+        View square2 = findViewById(R.id.green_square2);
+        View square3 = findViewById(R.id.green_square3);
+
+        square1.setBackgroundResource(R.drawable.neon_gray_square);
+        square2.setBackgroundResource(R.drawable.neon_gray_square);
+        square3.setBackgroundResource(R.drawable.neon_gray_square);
+
+        int buttonId = v.getId();
+        if (buttonId == R.id.button1) {
+            Toast.makeText(this, "Нажата кнопка 1 (Красный)", Toast.LENGTH_SHORT).show();
+            square1.setBackgroundResource(R.drawable.neon_red_square);
+            color = "red";
+        } else if (buttonId == R.id.button2) {
+            Toast.makeText(this, "Нажата кнопка 2 (Желтый)", Toast.LENGTH_SHORT).show();
+            square2.setBackgroundResource(R.drawable.neon_yellow_square);
+            color = "yellow";
+        } else if (buttonId == R.id.button3) {
+            Toast.makeText(this, "Нажата кнопка 3 (Зеленый)", Toast.LENGTH_SHORT).show();
+            square3.setBackgroundResource(R.drawable.neon_green_square);
+            color = "green";
+        }
+    }
+
+    private void saveSelectedColor(String color) {
+        if (color.isEmpty()) {
+            Toast.makeText(this, "Цвет не выбран!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        SharedPreferences.Editor editor = screenData.edit();
+        editor.putString(COLOR_KEY, color);
+        editor.apply();
+        Toast.makeText(this, "Цвет сохранен: " + color, Toast.LENGTH_SHORT).show();
+    }
+
+    private void applySavedColor(String color) {
+        if (color.isEmpty()) {
+            Toast.makeText(this, "Сохраненный цвет отсутствует!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        View square1 = findViewById(R.id.green_square1);
+        View square2 = findViewById(R.id.green_square2);
+        View square3 = findViewById(R.id.green_square3);
+
+        square1.setBackgroundResource(R.drawable.neon_gray_square);
+        square2.setBackgroundResource(R.drawable.neon_gray_square);
+        square3.setBackgroundResource(R.drawable.neon_gray_square);
+
+        switch (color) {
+            case "red":
+                square1.setBackgroundResource(R.drawable.neon_red_square);
+                break;
+            case "yellow":
+                square2.setBackgroundResource(R.drawable.neon_yellow_square);
+                break;
+            case "green":
+                square3.setBackgroundResource(R.drawable.neon_green_square);
+                break;
+            default:
+                break;
+        }
+        Toast.makeText(this, "Загружен цвет: " + color, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(COLOR_KEY, color);
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+
+        }
+    }
+    public void handleClose(View v) {
+        finish();
+    }
+}
+
+
+
+
+/*
+package com.example.portandlandlayout;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
@@ -72,15 +225,15 @@ public class MainActivity extends AppCompatActivity {
         if (buttonId == R.id.button1) {
             Toast.makeText(this, "Нажата кнопка 1 (Желтый)", Toast.LENGTH_SHORT).show();
             square1.setBackgroundResource(R.drawable.neon_red_square);
-            saveSelectedColor("red");
+       //          saveSelectedColor("red");
         } else if (buttonId == R.id.button2) {
             Toast.makeText(this, "Нажата кнопка 2 (Синий)", Toast.LENGTH_SHORT).show();
             square2.setBackgroundResource(R.drawable.neon_yellow_square);
-            saveSelectedColor("yellow");
+       //     saveSelectedColor("yellow");
         } else if (buttonId == R.id.button3) {
             Toast.makeText(this, "Нажата кнопка 3 (Зеленый)", Toast.LENGTH_SHORT).show();
             square3.setBackgroundResource(R.drawable.neon_green_square);
-            saveSelectedColor("green");
+           // saveSelectedColor("green");
         }
     }
 
@@ -106,7 +259,7 @@ public class MainActivity extends AppCompatActivity {
                 square3.setBackgroundResource(R.drawable.neon_green_square);
                 break;
             default:
-                // No saved color
+
                 break;
         }
     }
@@ -170,3 +323,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+*/

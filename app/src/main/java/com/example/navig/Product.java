f92353e0ab1162d7355e562a6012af893c115245
nikitazapekin <wotblitz362@mail.ru>
2024-10-06 22:08:43 +0300

    add pizzas

---
+++ app/src/main/java/com/example/navig/Product.java
@@ -0,0 +1,56 @@
+ package com.example.navig;
+
+public class Product {
+    private int imageResId;
+    private String name;
+    private double price;
+    public Product(int imageResId, String name, double price) {
+        this.imageResId = imageResId;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getImageResId() {
+        return imageResId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
+
+
+/*
+package com.example.navig;
+
+public class Product {
+    private int imageResId;
+    private String name;
+    private double price;
+
+    public Product(int imageResId, String name, double price) {
+        this.imageResId = imageResId;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getImageResId() {
+        return imageResId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
+ */
+

1f28f21c4de0c9c3154e4166dabbf327be4e2ae4
nikitazapekin <wotblitz362@mail.ru>
2024-10-06 15:05:19 +0300

    last

---
+++ app/src/main/java/com/example/navig/Main.java
@@ -0,0 +1,68 @@
+package com.example.navig;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Main extends Fragment {
+
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+
+    private String mParam1;
+    private String mParam2;
+
+    public Main() {
+
+    }
+
+
+    public static Main newInstance(String param1, String param2) {
+        Main fragment = new Main();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Click", Toast.LENGTH_SHORT).show();
+                Navigation.findNavController(view)
+
+                        .navigate(R.id.action_main2_to_second);
+            }
+        });
+
+        return view;
+    }
+
+}

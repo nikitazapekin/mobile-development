7b6cbf4728a80e80b35d3cf9fb24005770774520
nikitazapekin <wotblitz362@mail.ru>
2024-10-12 11:18:19 +0300

    last

---
+++ app/build.gradle.kts
@@ -0,0 +1,103 @@
+
+plugins {
+    alias(libs.plugins.android.application)
+
+
+
+    id("androidx.navigation.safeargs")
+    // apply plugin: "androidx.navigation.safeargs"
+
+}
+
+android {
+    namespace = "com.example.lab_6"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.lab_6"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    //  implementation("androidx.navigation:navigation-fragment:2.8.2")
+
+
+    val nav_version="2.5.0"
+    implementation("androidx.navigation:navigation-fragment:$nav_version")
+
+}
+
+
+/*plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.lab_6"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.lab_6"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
+*/
---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+}
+
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.0")
+    }
+
+
+}
+
+
+/*
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+}
+
+*/

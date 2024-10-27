cd487590c6f45d4d6d98784fb9c4fda39f9e8358
nikitazapekin <wotblitz362@mail.ru>
2024-10-27 14:10:06 +0300

    add first

--- app/src/main/java/com/example/lab7/First.java
+++ app/src/main/java/com/example/lab7/First.java
@@ -2,6 +2,7 @@ package com.example.lab7;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -46,11 +47,11 @@ public class First extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-      //  return inflater.inflate(R.layout.fragment_first, container, false);
+
 
         View view  = inflater.inflate(R.layout.fragment_first, container, false);
         MaterialToolbar toolbar = view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
         return view;
     }
 }

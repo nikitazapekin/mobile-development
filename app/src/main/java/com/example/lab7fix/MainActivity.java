fcf6fb583060d8b66253052d5ee3370a19ecea81
nikitazapekin <wotblitz362@mail.ru>
2024-10-29 12:05:09 +0300

    add logout

--- app/src/main/java/com/example/lab7fix/MainActivity.java
+++ app/src/main/java/com/example/lab7fix/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.lab7fix;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +23,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
+
+    //    MaterialToolbar toolbar = findViewById(R.id.toolbar);
+     //   setSupportActionBar(toolbar);
+    //    NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+   //             .findFragmentById(R.id.nav_host_fragment);
+   //     NavController navController = navHostFragment.getNavController();
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
+/*
+    @Override
+    public  boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return  super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        navController.navigate(item.getItemId());
+        return super.onOptionsItemSelected(item);
+    }
+
+ */
 }
--- app/src/main/java/com/example/lab7fix/Second.java
+++ app/src/main/java/com/example/lab7fix/Second.java
@@ -1,12 +1,127 @@
 package com.example.lab7fix;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.example.lab7fix.R;
+import com.google.android.material.appbar.MaterialToolbar;
+
+public class Second extends Fragment {
+
+    private TextView textView;
+    private EditText editText;
+
+    public Second() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true); // Indicate that this fragment has its own menu
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+
+        Button buttonAdd = view.findViewById(R.id.register_button1);
+        Button buttonReverse = view.findViewById(R.id.register_button);
+        textView = view.findViewById(R.id.textView2);
+        editText = view.findViewById(R.id.nameET);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleAdd(view);
+            }
+        });
+
+        buttonReverse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleReverse(view);
+            }
+        });
+
+        MaterialToolbar toolbar = view.findViewById(R.id.toolbarSecond);
+        ((AppCompatActivity) requireActivity()).setSupportActionBar(toolbar);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getChildFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+        }
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_toolbar, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
+        navController.navigate(item.getItemId());
+        return super.onOptionsItemSelected(item);
+    }
+
+    void handleAdd(View v) {
+        String text = editText.getText().toString().trim();
+        textView.setText(text);
+    }
+
+    void handleReverse(View v) {
+        String text = textView.getText().toString().trim();
+        if (!text.isEmpty()) {
+            String[] words = text.split(" ");
+            StringBuilder reversedText = new StringBuilder();
+
+            for (int i = words.length - 1; i >= 0; i--) {
+                reversedText.append(words[i]);
+                if (i != 0) {
+                    reversedText.append(" ");
+                }
+            }
+
+            textView.setText(reversedText.toString());
+        }
+    }
+}
+
+
+/*
+package com.example.lab7fix;
 
 import android.graphics.Color;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,6 +131,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.example.lab7fix.R;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class Second extends Fragment {
 
@@ -58,6 +174,18 @@ public class Second extends Fragment {
             }
         });
 
+
+        MaterialToolbar toolbar = view.findViewById(R.id.toolbarSecond);
+        ((AppCompatActivity) requireActivity()).setSupportActionBar(toolbar);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getChildFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+        }
+
+
+
         return view;
     }
  void handleAdd(View v) {
@@ -65,12 +193,6 @@ public class Second extends Fragment {
      textView.setText(text);
 
  }
-    /*
- void handleReverse(View v) {
-
- }
-
-     */
 
 
     void handleReverse(View v) {
@@ -90,8 +212,20 @@ public class Second extends Fragment {
             textView.setText(reversedText.toString());
         }
     }
+    @Override
+    public  boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return  super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+        navController.navigate(item.getItemId());
+        return super.onOptionsItemSelected(item);
+    }
 }
+*/
 /*
     @Override
     public void onCreateContextMenu(@NonNull ContextMenu menu, @NonNull View v, ContextMenu.ContextMenuInfo menuInfo) {

a75bf54132f2a2389549aac451ea5982395f42ba
nikitazapekin <wotblitz362@mail.ru>
2024-11-03 00:09:36 +0300

    add userData

--- app/src/main/java/com/example/lab9/DataController.java
+++ app/src/main/java/com/example/lab9/DataController.java
@@ -1,7 +1,10 @@
 package com.example.lab9;
 
+import androidx.annotation.Nullable;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.Transformations;
 import androidx.arch.core.util.Function;
 
@@ -10,6 +13,8 @@ import kotlin.jvm.functions.Function1;
 public class DataController {
 
     private static DataController instance;
+
+    private final MutableLiveData<Long> liveDataId = new MutableLiveData<>();
     private final MutableLiveData<String> liveData = new MutableLiveData<>();
 
     public static DataController getInstance() {
@@ -41,5 +46,72 @@ public class DataController {
         });
     }
 
+
+
+    public static class User {
+        // Поля и методы класса User
+    }
+
+
+    private LiveData<User> getUser(Long id) {
+        MutableLiveData<User> userLiveData = new MutableLiveData<>();
+        // Получение данных пользователя (например, из базы данных)
+        // userLiveData.setValue(...); // Установите значение пользователя
+        return userLiveData;
+    }
+    public LiveData<User> liveDataUser = (LiveData<User>) Transformations.switchMap(liveDataId, new Function<Long, LiveData<User>>() {
+        @Override
+        public LiveData<User> apply(Long id) {
+            return getUser(id);
+        }
+
+
+        public void setUserId(Long id) {
+            liveDataId.setValue(id);
+        }
+
+
+
+
+    });
+
+    /*
+    LiveData<Long> liveDataId =
+
+            LiveData<User> liveDataUser = Transformations.switchMap(liveDatalId,
+                    new Function<Long, LiveData<User>>() {
+
+                        @Override
+                        public LiveData<User> apply(Long id) {
+                            return getUser(id);
+
+                        }
+                    });
+*/
+
+/*
+    MutableLiveData<String> liveDatal = new MutableLiveData<>();
+    MutableLiveData<String> liveData2 = new MutableLiveData<>();
+
+    MediatorLiveData<String> mediatorLiveData = new MediatorLiveData<>();
+
+mediatorLiveData.addSource(liveDatal, new Observer<String>() {
+        @Override
+        public void onChanged(@Nullable String s) {
+            mediatorLiveData.setValue(s);
+        }
+
+
+        mediatorLiveData.addSource(liveData2, new Observer<String>() {
+
+            @Override
+
+            public void onChanged(@Nullable String s) {
+                mediatorLiveData.setValue(s);
+
+            }
+        });
+
+ */
 
 }

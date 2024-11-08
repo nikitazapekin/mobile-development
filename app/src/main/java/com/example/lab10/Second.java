95bb9cc2037cde075153f05edd1c79e324137e08
nikitazapekin <wotblitz362@mail.ru>
2024-11-09 00:40:22 +0300

    add pagination

--- app/src/main/java/com/example/lab10/Second.java
+++ app/src/main/java/com/example/lab10/Second.java
@@ -4,6 +4,113 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class Second extends Fragment {
+
+    private RecyclerView recyclerView;
+    private PostAdapter adapter;
+    private List<Post> posts = new ArrayList<>();
+    private int currentPage = 1;
+    private final int limit = 10;
+    private boolean isLoading = false;
+    private Button prevButton, nextButton;
+    private TextView pageNumber;
+
+    public Second() {}
+
+    public static Second newInstance(String param1, String param2) {
+        Second fragment = new Second();
+        Bundle args = new Bundle();
+        args.putString("param1", param1);
+        args.putString("param2", param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new PostAdapter(posts);
+        recyclerView.setAdapter(adapter);
+
+        prevButton = view.findViewById(R.id.prevButton);
+        nextButton = view.findViewById(R.id.nextButton);
+        pageNumber = view.findViewById(R.id.pageNumber);
+
+
+        loadPosts(currentPage);
+
+        prevButton.setOnClickListener(v -> {
+            if (currentPage > 1) {
+                currentPage--;
+                loadPosts(currentPage);
+            }
+        });
+
+        nextButton.setOnClickListener(v -> {
+            currentPage++;
+            loadPosts(currentPage);
+        });
+
+        return view;
+    }
+
+    private void loadPosts(int page) {
+        isLoading = true;
+        pageNumber.setText("Page " + page);
+        posts.clear(); // Clear the list for new data
+
+        NetworkService.getInstance()
+                .getJSONApi()
+                .getPosts(page, limit)
+                .enqueue(new Callback<List<Post>>() {
+                    @Override
+                    public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                        if (response.isSuccessful() && response.body() != null) {
+                            posts.addAll(response.body());
+                            adapter.notifyDataSetChanged();
+                        }
+                        isLoading = false;
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<Post>> call, Throwable t) {
+                        t.printStackTrace();
+                        isLoading = false;
+                    }
+                });
+    }
+}
+
+/*
+package com.example.lab10;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -93,57 +200,4 @@ public class Second extends Fragment {
                 });
     }
 }
-
-/*
-package com.example.lab10;
-
-import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-
-public class Second extends Fragment {
-
-
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Second() {
-        // Required empty public constructor
-    }
-
-
-    public static Second newInstance(String param1, String param2) {
-        Second fragment = new Second();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
-    }
-}
-*/
+ */

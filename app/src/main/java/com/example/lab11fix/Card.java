877daf82cf779ad383d4e365d3a618603fce242e
nikitazapekin <wotblitz362@mail.ru>
2024-11-18 18:11:17 +0300

    last

--- app/src/main/java/com/example/lab11fix/Card.java
+++ app/src/main/java/com/example/lab11fix/Card.java
@@ -8,19 +8,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Card#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Card extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
+    private final String ts = "1";
+    private final String apiKey = "23f31debdba1e947455a5b78a77e7e56";
+    private final String hash = "5ef5fd12a0184b988b9f558f5a852eb6";
+
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +27,7 @@ public class Card extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Card.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static Card newInstance(String param1, String param2) {
         Card fragment = new Card();
         Bundle args = new Bundle();

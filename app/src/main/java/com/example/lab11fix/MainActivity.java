2626fb3b2f3f3528986aad048a7010f6ded2d511
nikitazapekin <wotblitz362@mail.ru>
2025-01-20 02:45:01 +0300

    last

--- app/src/main/java/com/example/lab11fix/MainActivity.java
+++ app/src/main/java/com/example/lab11fix/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //  setContentView(R.layout.activity_main);
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
